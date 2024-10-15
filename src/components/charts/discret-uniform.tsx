b33c50880233059a444d2c540613259751726b37
Ender Puentes <endpuent@gmail.com>
2024-10-15 19:28:36 -0300

    feat(chars/DiscretUniform): refactor logic and ui

--- src/components/charts/discret-uniform.tsx
+++ src/components/charts/discret-uniform.tsx
@@ -1,38 +1,59 @@
 'use client';
 
+import { useToast } from '@/hooks/use-toast';
 import { BarChart } from '@tremor/react';
 import { useEffect, useState } from 'react';
-import { Card, CardContent, CardHeader } from '../ui/card';
+import { Card, CardContent, CardHeader, CardTitle } from '../ui/card';
 import { Input } from '../ui/input';
 import { Label } from '../ui/label';
 
-const generateUniformProbabilityDistribution = (min: number, max: number) => {
-  const range = max - min + 1;
-  const probability = 1 / range;
-
-  return Array.from({ length: range }, (_, index) => ({
-    category: `${min + index}`,
-    value: probability,
-  }));
-};
-
 export const DiscretUniformChar: React.FC = () => {
+  const { toast } = useToast();
+
   const [minValue, setMinValue] = useState<number>(1);
-  const [maxValue, setMaxValue] = useState<number>(10);
+  const [maxValue, setMaxValue] = useState<number>(6);
+
+  const [probability, setProbability] = useState(0);
   const [data, setData] = useState<{ category: string; value: number }[]>([]);
 
+  const generateDiscreteUniformProbability = (min: number, max: number) => {
+    if (min > max) {
+      return [];
+    }
+
+    const range = max - min + 1;
+    const probability = 1 / range;
+
+    setProbability(probability);
+
+    return Array.from({ length: range }, (_, index) => ({
+      category: `${min + index}`,
+      value: probability,
+    }));
+  };
+
   useEffect(() => {
-    const distributionData = generateUniformProbabilityDistribution(
+    const distributionData = generateDiscreteUniformProbability(
       minValue,
       maxValue
     );
+
+    if (!distributionData.length) {
+      toast({
+        title: "'a' must be less than or equal to 'b'.",
+        variant: 'destructive',
+      });
+    }
+
     setData(distributionData);
-  }, [minValue, maxValue]);
+  }, [minValue, maxValue, toast]);
 
   return (
     <Card>
       <CardHeader>
-        <h3 className="text-xl font-semibold mb-10">{`DU(min=${minValue}, max=${maxValue})`}</h3>
+        <CardTitle className="text-xl font-semibold mb-10">{`DU(min:${minValue}, max:${maxValue}) = ${probability.toFixed(
+          4
+        )}`}</CardTitle>
         <div className="flex justify-between items-center gap-5">
           <div className="flex justify-start items-center gap-3 w-full">
             <Label className="font-bold text-lg w-[200px]">Min Value:</Label>
@@ -42,6 +63,7 @@ export const DiscretUniformChar: React.FC = () => {
               value={minValue}
               onChange={(e) => setMinValue(Number(e.target.value))}
               min={0}
+              max={maxValue}
             />
           </div>
           <div className="flex justify-start items-center gap-3 w-full">
@@ -51,7 +73,7 @@ export const DiscretUniformChar: React.FC = () => {
               name="maxValue"
               value={maxValue}
               onChange={(e) => setMaxValue(Number(e.target.value))}
-              min={0}
+              min={minValue}
             />
           </div>
         </div>
@@ -66,6 +88,7 @@ export const DiscretUniformChar: React.FC = () => {
           yAxisWidth={70}
           showGridLines={true}
           showAnimation={true}
+          valueFormatter={(value) => value.toFixed(4)}
         />
       </CardContent>
     </Card>
